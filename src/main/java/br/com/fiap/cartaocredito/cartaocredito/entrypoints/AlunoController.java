4bbaf9b536d0bbf884e52177780e699c33d26cd0
Bruno Farias <contato.brunofarias@gmail.com>
2020-03-12 23:38:30 -0300

    Adicionando erro nos retornos.

--- src/main/java/br/com/fiap/cartaocredito/cartaocredito/entrypoints/AlunoController.java
+++ src/main/java/br/com/fiap/cartaocredito/cartaocredito/entrypoints/AlunoController.java
@@ -1,7 +1,6 @@
 package br.com.fiap.cartaocredito.cartaocredito.entrypoints;
 
 import br.com.fiap.cartaocredito.cartaocredito.domain.Aluno;
-import br.com.fiap.cartaocredito.cartaocredito.domain.AlunoRepository;
 import br.com.fiap.cartaocredito.cartaocredito.domain.AlunoService;
 import br.com.fiap.cartaocredito.cartaocredito.domain.ImportacaoArquivoAlunosService;
 import org.springframework.http.HttpStatus;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @RestController
@@ -25,26 +23,23 @@ public class AlunoController {
     }
 
     @GetMapping("{rm}")
-    public AlunoDto obtemAlunoPorRm(@PathVariable Long rm){
-        try{
+    public AlunoDto obtemAlunoPorRm(@PathVariable Long rm) {
+        try {
             Aluno aluno = alunoService.buscaAlunoPorRm(rm);
-            return new AlunoDto(aluno.getRm(), aluno.getNome(),aluno.getNumeroCartao(), aluno.getDigitoVerificadorCartao());
-        }
-        catch (Exception e){
+            return new AlunoDto(aluno.getRm(), aluno.getNome(), aluno.getNumeroCartao(), aluno.getDigitoVerificadorCartao());
+        } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
         }
     }
 
-    //Contrato consiste em enviar o arquivo no formato especifico
     @PostMapping
     public void importaAlunos(@RequestParam("file") MultipartFile file) {
 
         try {
             String conteudoArquivo = new String(file.getBytes(), StandardCharsets.UTF_8);
             importacaoArquivoAlunosService.importaAlunos(conteudoArquivo);
-        } catch (IOException e) {
-            //Todo - Tratar melhor os retornos.
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Ocorreu um erro na importação do arquivo.", e);
         }
     }
 }
