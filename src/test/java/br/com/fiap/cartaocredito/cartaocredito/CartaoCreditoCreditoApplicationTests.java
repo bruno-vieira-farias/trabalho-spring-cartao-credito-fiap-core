6fe19b4bf21f5564d30f7ff2f81ea72afae3cb0b
serginhoh <serginhoh@gmail.com>
2020-03-28 09:03:51 -0300

    Testes Aluno / Transacao

--- src/test/java/br/com/fiap/cartaocredito/cartaocredito/CartaoCreditoCreditoApplicationTests.java
+++ src/test/java/br/com/fiap/cartaocredito/cartaocredito/CartaoCreditoCreditoApplicationTests.java
@@ -1,54 +1,100 @@
 package br.com.fiap.cartaocredito.cartaocredito;
 
+import br.com.fiap.cartaocredito.cartaocredito.domain.entity.Aluno;
 import br.com.fiap.cartaocredito.cartaocredito.domain.repository.AlunoRepository;
-import br.com.fiap.cartaocredito.cartaocredito.entrypoints.aluno.AlunoController;
 import br.com.fiap.cartaocredito.cartaocredito.entrypoints.aluno.AlunoDto;
+import br.com.fiap.cartaocredito.cartaocredito.entrypoints.transacao.StatusTransacaoDto;
+import br.com.fiap.cartaocredito.cartaocredito.entrypoints.transacao.TransacaoDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.math.BigDecimal;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-//@ContextConfiguration(classes = CartaoCreditoApplication.class)
-//@WebMvcTest(controllers = AlunoController.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class CartaoCreditoCreditoApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-	@MockBean
-	private AlunoRepository alunoRepository;
+    @Autowired
+    private AlunoRepository alunoRepository;
 
-	@Test
-	void contextLoads() throws  Exception{
+    @Test
+    void contextLoads() throws Exception {
 
+        //AlunoController
+        long rm = -99999;
+        String codigoTurma = "35SCJ";
+        Aluno aluno = alunoRepository.findByRm(rm);
 
-		AlunoDto aluno = new AlunoDto(99999L, "Teste Post");
+        if (aluno != null) {
+            alunoRepository.deleteById(rm);
+        }
 
-		mockMvc.perform(post("/aluno")
-				.contentType("application/json")
-				.content(objectMapper.writeValueAsString(aluno)))
-				.andExpect(status().isCreated());
+        AlunoDto alunoDto = new AlunoDto(rm, "Teste Post", codigoTurma);
 
-			mockMvc.perform(get("/aluno/{rm}", 99999))
-				.andDo(print()).andExpect(status().isOk());
+        //Post novo aluno
+        mockMvc.perform(post("/aluno")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(alunoDto)))
+                .andExpect(status().isCreated());
+
+        //Get aluno por Rm
+        mockMvc.perform(get("/aluno/{rm}", rm))
+                .andDo(print()).andExpect(status().isOk());
+
+        //Get alunos por Turma
+        mockMvc.perform(get("/aluno/codigo-turma/{codigoTurma}", codigoTurma))
+                .andDo(print()).andExpect(status().isOk());
+
+        //Put Aluno
+        alunoDto.setNome("Teste Put");
+        mockMvc.perform(put("/aluno")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(alunoDto)))
+                .andExpect(status().isOk());
+
+		//Delete Aluno
+		mockMvc.perform(delete("/aluno/{rm}", rm)).andExpect(status().isOk());
+
+        //TransacoesController
+        Integer id = -99999;
+		rm = -99998;
+        ZonedDateTime data = ZonedDateTime.ofInstant(new Date().toInstant(), ZoneOffset.systemDefault());
+        TransacaoDto transacaoDto = new TransacaoDto(id, data, new BigDecimal("1500.99"), StatusTransacaoDto.AUTORIZADA, "Aut -99999", rm);
+		aluno = alunoRepository.findByRm(rm);
+
+		if (aluno == null) {
+            aluno = new Aluno(rm, "Teste Post", codigoTurma);
+			alunoRepository.save(aluno);
+		}
+
+        //Post Registra transação
+        mockMvc.perform(post("/transacao")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(transacaoDto)))
+                .andExpect(status().isOk());
+
+		//Get Transação por ID
+        mockMvc.perform(get("/transacao/{id}", id))
+                .andDo(print()).andExpect(status().isOk());
 	}
 }
